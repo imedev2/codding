85e3db3c73943aa38a9bf2fb4342373197abe8aa
fatima <imedev2@Imedev2s-MacBook-Pro.local>
2018-05-22 14:11:03 +0300

    create post with photo and dynamic category

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -23,7 +23,7 @@
         <div class="form-group">
 
             {!! Form::label('category_id','Category:') !!}
-            {!! Form::select('category_id',array(1=>'PHP', 2=>'JavaScript') ,null,['class'=>'form-control']) !!}
+            {!! Form::select('category_id',[''=>'choose category']+ $categories ,null,['class'=>'form-control']) !!}
 
         </div>
 
